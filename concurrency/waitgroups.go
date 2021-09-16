74e6e77987e3309ee99971cd79b9b009364b55ec
Gregori Piñeres <gregori.pineres02@gmail.com>
2021-09-16 09:10:01 -0400

    Add wg

--- concurrency/waitgroups.go
+++ concurrency/waitgroups.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	fmt.Println("wg")
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		// wg.Add incrementa en 1 la cantidad de rutinas agregadas al wg.
+		wg.Add(1)
+		go sleeping(i, &wg)
+	}
+
+	wg.Wait()
+}
+
+func sleeping(index int, wg *sync.WaitGroup) {
+	// defer se ejecuta cuando la rutina es terminada.
+	// wg.Done derementa en 1 la cantidad de rutinas agregadas al wg.
+	defer wg.Done()
+	fmt.Printf("[sleeping]: starting %d", index)
+	time.Sleep(2 * time.Second)
+	fmt.Printf("[sleeping]: done %d", index)
 }
